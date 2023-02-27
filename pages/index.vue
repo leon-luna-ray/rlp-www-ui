7c05f2246eea2738a31889964c8d6f5f3946de71
Ray Luna <leon.luna.ray@gmail.com>
2023-02-27 11:42:09 -0800

    setup initial api call

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <script setup>
-const data = await useFetch('http://localhost:8000/api/v2/pages/')
-
+const reqUrl = `${import.meta.env.VITE_APP_API_BASE_URL}/pages/${import.meta.env.VITE_APP_HOME_PAGE_ID}`
+const { data } = await useFetch(reqUrl);
 </script>
 <template>
-    <div class="container">
+    <div v-if="data" class="container">
         <div class="page-title">
-            <h1>Home</h1>
-            <h2>Subtitle</h2>
+            <h1>{{ data.title }}</h1>
+            <h2>{{ data.subtitle }}</h2>
         </div>
-        <div class="intro">Intro</div>
+        <div class="intro" v-html="data.intro"></div>
+        <img :src="data.hero_image.thumbnail" :alt="data.hero_image.alt_text">
     </div>
 </template>
