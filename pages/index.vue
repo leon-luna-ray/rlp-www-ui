e91230889002869ae33f3dfe09a1bdd282e77409
Ray Luna <leon.luna.ray@gmail.com>
2023-02-27 22:00:00 -0800

    add hover effect to image grid

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ const reqUrl = `${import.meta.env.VITE_APP_API_BASE_URL}/pages/${import.meta.env
 const { data } = await useFetch(reqUrl);
 </script>
 <template>
-    <div v-if="data" class="container p-5 flex flex-col gap-y-5">
+    <div v-if="data" class="p-5 flex flex-col gap-y-5">
         <div class="header">
             <div class="page-title">
                 <h1>{{ data.title }}</h1>
@@ -11,8 +11,8 @@ const { data } = await useFetch(reqUrl);
             </div>
             <div v-if="data.intro" v-html="data.intro"  class="intro"></div>
         </div>
-        <div class="image-grid grid md:gird-cols-2 lg:grid-cols-3 gap-5 justify-between w-full">
-            <div v-for="image in data.page_content[0].value.images" class="img-wrap w-full">
+        <div class="image-grid">
+            <div v-for="image in data.page_content[0].value.images" class="image-wrap">
                 <img :src="image.value.thumbnail" :alt="image.value.alt_text">
             </div>
         </div>
